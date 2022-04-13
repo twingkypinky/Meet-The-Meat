0215330720e28066ec18389765489c541751aaad
RafaellVictor <90367065+RafaellVictor@users.noreply.github.com>
2022-04-13 21:58:51 +0700

    Add files via upload

--- Meet The Meat/src/OrderNew.java
+++ Meet The Meat/src/OrderNew.java
@@ -1,15 +1,21 @@
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.converter.NumberStringConverter;
@@ -17,6 +23,8 @@ import javafx.util.converter.NumberStringConverter;
 public class OrderNew implements Initializable{
 
     MenuItem selected;
+
+    Connection conn;
 
     @FXML
     private AnchorPane apOrderNew;
@@ -94,8 +102,30 @@ public class OrderNew implements Initializable{
             // jika terdapat item yg dipilih, maka tambah qty dari item tersebut
             int id = selected.getId();
             Node node = Data.get(id);
-            node.setQty(node.getQty() + 1);
-            Data.updateOrderData();
+
+            int afterAdd = selected.getQty() + 1;
+
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/burgerapp", "root", "240122");
+
+                String sql = "SELECT Stock FROM Inventory WHERE FoodName = '" + selected.getName() + "'";
+                Statement st = conn.createStatement();
+                ResultSet rs = st.executeQuery(sql);
+
+                while (rs.next()) {
+                    if (afterAdd <= rs.getInt("Stock")) {
+                        node.setQty(node.getQty() + 1);
+                        Data.updateOrderData();
+                    }
+                }
+            } catch (Exception e) {
+                Alert error = new Alert(AlertType.ERROR);
+                error.setTitle("Error Dialog");
+                error.setHeaderText("An Error Has Occurred");
+                error.setContentText("Incorrect name or password!");
+                error.showAndWait();
+            }
         }
     }
 
