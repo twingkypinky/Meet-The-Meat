4d19b7a1d293dda6cec6cfe4a0951483fc92b038
RafaellVictor <90367065+RafaellVictor@users.noreply.github.com>
2022-04-17 12:41:04 +0700

    Add files via upload

--- Meet The Meat/src/AfterOrder.java
+++ Meet The Meat/src/AfterOrder.java
@@ -2,10 +2,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
 public class AfterOrder {
     Connection conn;
@@ -30,6 +32,8 @@ public class AfterOrder {
 
     @FXML
     private TextField tfTotalAfterOrder;
+    
+    ObservableList<MenuItem> orders = Data.getOrderData();
 
     public void initialize() {
         
@@ -48,12 +52,18 @@ public class AfterOrder {
                 stringPhone.setText(rs.getString("Phone"));
                 tfTotalAfterOrder.setText("" + Data.getTotalPriceProperty().intValue());
                 tfQtyAfterOrder.setText("" + Data.getTotal());
-                //Tinggal yng untuk tfItemAfterOrder 
-                 
-            }
-            //rs.close();
-        } catch (Exception e) {
 
+                for (int i = 0; i < 1; i++) {
+                    orders.forEach((orders) -> {tfItemAfterOrder.appendText(orders.getName() + ", ");});
+                }
+            }
+
+        } catch (Exception e) {
+            Alert error = new Alert(AlertType.ERROR);
+            error.setTitle("Error Dialog");
+            error.setHeaderText("An Error Has Occurred");
+            error.setContentText("Error! Please contact Customer Service!");
+            error.showAndWait();
         }
     }
 
